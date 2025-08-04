9203b213ddb6755d0229dfc6d3347e95610c71d7
Malena Lucero <luceromalena@hotmail.com>
2025-08-03 23:02:07 -0300

    fix method not allowed

--- pages/api/subtitles/lines/filter.ts
+++ pages/api/subtitles/lines/filter.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 
 export default async (req: any, res: any) => {
    await NextCors(req, res, {
-      methods: ['POST'],
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       origin: '*',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
    });
