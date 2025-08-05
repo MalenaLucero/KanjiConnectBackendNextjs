a3e504ae6e3276410c24203e0f3b41fd5d04d1f4
Malena Lucero <luceromalena@hotmail.com>
2025-08-04 21:48:51 -0300

    filter words in subtitles with GET method

--- pages/api/subtitles/filter.ts
+++ pages/api/subtitles/filter.ts
@@ -13,19 +13,14 @@ export default async (req: any, res: any) => {
    try {
         const client = await clientPromise;
         const db = client.db("kanji-connect");
-
-        const { body } = req;
-        const words = body.words;
-        const animeIds = body?.animeIds;
+        const words = req.query.words.split(',');
+        const anime = await db.collection("anime").find({}).toArray();
 
         const allResults = [];
    
         for(let word of words) {
             const match: any = {
                 "subtitles.line": { $regex: word }
-            }
-            if (animeIds) {
-                match.anime = { $in: animeIds.map((id: string) => new ObjectId(id))}
             }
             const filter = await db.collection('episodes').find(match).toArray();
  
@@ -36,29 +31,28 @@ export default async (req: any, res: any) => {
                 return { ...episode, subtitles: filteredSubtitles }
             });
 
-            let anime;
-            if (animeIds) {
-                anime = await db.collection("anime").find({_id: {$in: animeIds.map((id: string) => new ObjectId(id))}}).toArray();
-            } else {
-                anime = await db.collection("anime").find({}).toArray();
+            if (filteredEpisodes.length > 0) {
+                const result = {
+                    word,
+                    numberOfInstances,
+                    numberOfEpisodes: filter.length,
+                    episodesGroupedByAnime: anime.map(a => {
+                        return {
+                            anime: a.englishTitle,
+                            episodes: filteredEpisodes.filter((episode: any) => episode.anime.toString() === a._id.toString())
+                        }
+                    }).filter((anime: any) => anime.episodes.length > 0)
+                };
+
+                allResults.push(result);
             }
-
-            const result = {
-                word,
-                numberOfInstances,
-                numberOfEpisodes: filter.length,
-                episodesGroupedByAnime: anime.map(a => {
-                    return {
-                        anime: a.englishTitle,
-                        episodes: filteredEpisodes.filter((episode: any) => episode.anime.toString() === a._id.toString())
-                    }
-                }).filter((anime: any) => anime.episodes.length > 0)
-            };
-
-            allResults.push(result);
         }
 
-        res.json(allResults);
+        if (allResults.length > 0) {
+            res.json(allResults);
+        } else {
+            res.json('Word or words not found');
+        }
     } catch (e) {
         console.error(e);
     }
